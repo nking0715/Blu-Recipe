6309604054d316562f8f781f88894cb257980893
TalentedBlu123 <talentedblu@gmail.com>
2022-08-31 03:46:28 -0300

    login page missing btns

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './style/style.css';
+import '@fontsource/nunito-sans';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
