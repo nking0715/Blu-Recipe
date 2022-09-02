cf889548e62aa6621e574088a4f6cb71e72d6ea5
TalentedBlu123 <talentedblu@gmail.com>
2022-09-02 02:21:23 -0300

    continue the implementation of search page - missing retrieving data from mealdb

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -2,14 +2,20 @@ import { Link } from 'react-router-dom';
 import { countries } from '../apiMock/countries.js';
 
 function Cards() {
-  console.log(countries);
+  console.log(countries[0].flags.svg);
 
   const renderCards = () => {
     const markup = () => {
       return (
-        <figure>
+        <figure
+          style={{
+            background: `url('${countries[5].flags.svg}')`,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+          }}
+        >
           <Link to='/details'>
-            <img src='strArea' alt='country flag' />
+            <img src={countries[1].flags.svg} alt='country flag' />
             <div>
               <h4>Nome da receita</h4>
               <p>Categoria</p>
@@ -18,8 +24,8 @@ function Cards() {
         </figure>
       );
     };
-
     return markup();
+    // return array.map((recipe) => markup(recipe));
   };
 
   return renderCards();
