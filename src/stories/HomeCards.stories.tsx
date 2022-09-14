786a001cb028f873e35ace1f735ad2ac851f0887
TalentedBlu123 <talentedblu@gmail.com>
2022-09-14 16:15:24 -0300

    fix: fix HomeCards stories

--- src/stories/HomeCards.stories.tsx
+++ src/stories/HomeCards.stories.tsx
@@ -5,8 +5,13 @@ import '../style/style.css'
 export default {
   title: 'Components/HomeCards',
   component: HomeCards,
+  argTypes: {
+    onClick: dispatchEvent,
+  },
 } as ComponentMeta<typeof HomeCards>
 
-const Template: ComponentStory<typeof HomeCards> = () => <HomeCards />
+const Template: ComponentStory<typeof HomeCards> = (args) => (
+  <HomeCards {...args} />
+)
 
 export const Default = Template.bind({})
